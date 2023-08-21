e945cfc61ecd7be25d5d678259a90a1603262b96
Jananisaravanan57 <77255860+Jananisaravanan57@users.noreply.github.com>
2023-08-21 11:58:10 +0530

    Add files via upload

---
+++ Training/src/basics/ClassPath.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class ClassPath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String classpath = System.getProperty("java.class.path");
+		System.out.println(classpath);
+
+	}
+
+}
